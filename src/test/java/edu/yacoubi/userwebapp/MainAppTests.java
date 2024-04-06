2985215618204a0ab5ff203bf812a93a7506783a
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-04-06 04:38:02 +0200

    CRUD User module Thymeleaf project setup

---
+++ src/test/java/edu/yacoubi/userwebapp/MainAppTests.java
@@ -0,0 +1,13 @@
+package edu.yacoubi.userwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
