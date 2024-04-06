645c2fe349a7d2ace3e27a83f6fa58c79a87e744
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-04-07 00:38:36 +0200

    userform and save user

--- src/main/java/edu/yacoubi/userwebapp/user/UserController.java
+++ src/main/java/edu/yacoubi/userwebapp/user/UserController.java
@@ -4,17 +4,31 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class UserController {
-    private final UserRepository repository;
+    private final UserService userService;
 
     @GetMapping("/users")
     public String showUserList(Model model) {
-        Iterable<User> listUsers = repository.findAll();
+        Iterable<User> listUsers = userService.listAll();
         model.addAttribute("listUsers", listUsers);
-
         return "users";
+    }
+
+    @GetMapping("/users/new")
+    public String showNewUserForm (Model model) {
+        model.addAttribute("user", new User());
+        model.addAttribute("pageTitle", "Add New User");
+
+        return "user_form";
+    }
+
+    @PostMapping("/users/save")
+    public String saveUser(User user) {
+        userService.save(user);
+        return "redirect:/users";
     }
 }
